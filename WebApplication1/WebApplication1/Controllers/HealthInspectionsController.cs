f72829483a8146e9cfed2fa19ceb1e473f5caebf
jboi <jtwest06@louisville.edu>
2017-02-23 16:12:18 -0500

    initial website from jonathan

---
+++ WebApplication1/WebApplication1/Controllers/HealthInspectionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.DataContext;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication1.Controllers
+{
+    public class HealthInspectionsController : Controller
+    {
+        // GET: HealthInspections
+        public ActionResult Index()
+        {
+            GetJsonData var = new GetJsonData();
+            var jobject = JsonConvert.DeserializeObject<RootObject>(var.GetData().ToString());
+            return View(jobject.courses);
+        }
+    }
+}
